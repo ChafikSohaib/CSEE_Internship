f8686800146b35b1af503cb5ee40ab3faf2f18d2
ChafikSohaib <40965256+ChafikSohaib@users.noreply.github.com>
2018-07-11 17:07:20 +0100

    Add files via upload

---
+++ SPI_Master_Arduino.ino
@@ -0,0 +1,19 @@
+#include<SPI.h>
+
+void setup()
+{
+  pinMode(SS, OUTPUT);
+  digitalWrite(SS, HIGH);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV4);  //2^n = 2, 4, 8, 16, 32
+
+}
+
+
+void loop()
+{
+  digitalWrite(SS, LOW);
+  SPI.transfer('f');
+  SPI.transfer('\n');
+  digitalWrite(SS, HIGH);
+}
---
+++ SPI_Slave_Arduino.ino
@@ -0,0 +1,43 @@
+#include<SPI.h>
+
+char buf[5];
+volatile byte pos = 0;
+volatile boolean process_it = false; 
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("SPI test");
+  pinMode(MISO, OUTPUT);
+  digitalWrite(MISO, LOW);
+  SPCR |= _BV(SPE); //SPI Enable
+  SPCR |= _BV(SPIE);
+  //SPI.attachInterrupt();
+}
+
+
+ISR(SPI_STC_vect)
+{
+  byte c = SPDR;
+
+  if(pos < sizeof buf)
+  {
+    buf[pos++] = c;
+    if(c == '\n')
+     {
+        process_it = true;
+}
+} 
+}
+
+void loop()
+{
+    if(process_it)
+      {
+        Serial.println(buf);
+        pos = 0;
+        process_it = false;
+        delay (1000); 
+}
+}
