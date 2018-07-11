91220c665858e42346ad65462f218c840a05b38f
ChafikSohaib <40965256+ChafikSohaib@users.noreply.github.com>
2018-07-11 17:34:22 +0100

    Update SPI_Slave_Arduino.ino

--- SPI_Slave_Arduino.ino
+++ SPI_Slave_Arduino.ino
@@ -1,3 +1,5 @@
+// SPI Slave
+
 #include<SPI.h>
 
 char buf[5];
@@ -11,31 +13,30 @@ void setup()
   Serial.println("SPI test");
   pinMode(MISO, OUTPUT);
   digitalWrite(MISO, LOW);
-  SPCR |= _BV(SPE); //SPI Enable
-  SPCR |= _BV(SPIE);
-  //SPI.attachInterrupt();
+  SPCR |= _BV(SPE);     //SPI Enable
+  SPCR |= _BV(SPIE);    //SPI.attachInterrupt();
 }
 
 
-ISR(SPI_STC_vect)
+ISR(SPI_STC_vect)   // SPI interrupt routine
 {
-  byte c = SPDR;
+  byte c = SPDR;    // grab byte from SPI data register
 
-  if(pos < sizeof buf)
+  if(pos < sizeof buf)    // add the incoming byte c to the buffer in the next position
   {
     buf[pos++] = c;
     if(c == '\n')
      {
         process_it = true;
-}
-} 
-}
+     }
+  } 
+}       // end of SPI interrupt routine
 
 void loop()
 {
-    if(process_it)
+    if(process_it)            // printing the buffer by the slave
       {
-        Serial.println(buf);
+        Serial.println(buf);      
         pos = 0;
         process_it = false;
         delay (1000); 
