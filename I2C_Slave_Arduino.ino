63bc5a369276a52ab766eefd8ae691026af027bb
ChafikSohaib <40965256+ChafikSohaib@users.noreply.github.com>
2018-07-11 17:14:14 +0100

    Update I2C_Slave_Arduino.ino

--- I2C_Slave_Arduino.ino
+++ I2C_Slave_Arduino.ino
@@ -1,13 +1,5 @@
 // Wire Slave Receiver
-// by Nicholas Zambetti <http://www.zambetti.com>
 
-// Demonstrates use of the Wire library
-// Receives data as an I2C/TWI slave device
-// Refer to the "Wire Master Writer" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
 
 
 #include <Wire.h>
@@ -24,6 +16,7 @@ void loop() {
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
+
 void receiveEvent(int howMany) {
   while (1 < Wire.available()) { // loop through all but the last
     char c = Wire.read(); // receive byte as a character
