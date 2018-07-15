77464e51e682016e357e234a82b3f9938479a029
ChafikSohaib <40965256+ChafikSohaib@users.noreply.github.com>
2018-07-15 13:41:45 +0100

    Add files via upload

---
+++ main_I2C.c
@@ -0,0 +1,53 @@
+//I2C communication
+
+#include <msp430.h>
+unsigned char TXData;
+unsigned char TXByteCtr;
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P3SEL |= BIT1 + BIT2;                     // Assign I2C pins to USCI_B0
+
+  UCB0CTL1 |= UCSWRST;                      // Enable SW reset
+  UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     // I2C Master, synchronous mode
+  UCB0CTL1 = UCSSEL_2 + UCSWRST;            // Use SMCLK, keep SW reset
+  UCB0BR0 = 12;                             // fSCL = SMCLK/12 = ~100kHz
+  UCB0BR1 = 0;
+  UCB0I2CSA = 0x69;                         // Slave Address is 048h
+  UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
+  IE2 |= UCB0TXIE;                          // Enable TX interrupt
+
+  TXData = 0x34;                            // Holds TX data
+
+  while (1)
+  {
+    TXByteCtr = 1;                          // Load TX byte counter
+
+    UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
+    __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+                                            // Remain in LPM0 until all data
+                                            // is TX'd
+    TXData++;                               // Increment data byte
+  }
+}
+
+//------------------------------------------------------------------------------
+// The USCIAB0TX_ISR is structured such that it can be used to transmit any
+// number of bytes by pre-loading TXByteCtr with the byte count.
+//------------------------------------------------------------------------------
+#pragma vector = USCIAB0TX_VECTOR
+__interrupt void USCIAB0TX_ISR(void)
+{
+  if (TXByteCtr)                            // Check TX byte counter
+  {
+    UCB0TXBUF = TXData;                     // Load TX buffer
+    TXByteCtr--;                            // Decrement TX byte counter
+  }
+  else
+  {
+    UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+    IFG2 &= ~UCB0TXIFG;                     // Clear USCI_B0 TX int flag
+    __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
+  }
+}
